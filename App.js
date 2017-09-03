e90b2b8d637b3208fe9c997d7b657cb07292a6dd
Rakshit <rakshit@infrrd.ai>
2017-09-03 17:40:22 +0530

    Initializing create-react-native and setting up dev environment

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Aur bhai kya haal hai </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
